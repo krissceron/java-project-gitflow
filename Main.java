f17af4eb60a0a3498490d50e0f04aff9c84e4ee9
krissceron <krissceronb03@gmail.com>
2025-06-22 16:34:25 +0000

    Agregar soporte para múltiples idiomas en los saludos.

--- Main.java
+++ Main.java
@@ -6,7 +6,9 @@ public class Main {
         System.out.println("***************");
         System.out.println("* JAVA PROJECT *");
         System.out.println("***************");
-        System.out.println(saludo.generarSaludo("Mundo"));
+        System.out.println(saludo.generarSaludo("Mundo", "en"));
+        System.out.println(saludo.generarSaludo("Mundo", "es"));
+        System.out.println(saludo.generarSaludo("Mundo", "fr"));
         System.out.println(despedida.generarDespedida("Mundo"));
         System.out.println("Versión 1.0.0 lista para producción.");
     }
